a9dd94585e9290e920e55d9339f3217e99851bd2
Irving Silva <irving_s123@hotmail.com>
2021-11-16 19:23:44 -0600

    Celdas agregadas en lista

--- src/componentes/Lista/index.js
+++ src/componentes/Lista/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Celda from "../Celda";
 import "./Lista.css";
 
 function Lista() {
     return(
         <section className="lista">
-
+            <Celda />
+            <Celda />
+            <Celda />
+            <Celda />
         </section>
     );
 }
