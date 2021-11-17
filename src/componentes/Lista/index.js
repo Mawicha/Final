dd5ab0223baad82251ebd0087342cfe004b156e2
Mawicha <A01274427@itesm.mx>
2021-11-16 20:41:54 -0600

    Petición get en la lista

--- src/componentes/Lista/index.js
+++ src/componentes/Lista/index.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Celda from "../Celda";
 import "./Lista.css";
 
 function Lista() {
+    const [pokemon, setPokemon] = useState([]);
+    const pokeAPILink = "";
+
+    useEffect(() => {
+        const getPokemon = async() => {
+            const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+            const data = await response.json();
+            const listaPokemon = data.results
+                .slice(0, data.results.length)
+                .map(items => items)
+                .flat();
+            setPokemon(listaPokemon);
+        }
+        getPokemon();
+    }, []);
+
     return(
         <section className="lista">
-            <Celda />
-            <Celda />
-            <Celda />
-            <Celda />
+           {pokemon.map((pokeClase, id) => {
+               return(
+                   <Celda key={id} pokeClase={id} />
+               )
+           })}
         </section>
     );
 }
