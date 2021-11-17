354b9d21fe6b42874b6f20d731ca5bd37345f3ca
Mawicha <A01274427@itesm.mx>
2021-11-16 20:54:39 -0600

    Inserción de las imagenes

--- src/componentes/Celda/index.js
+++ src/componentes/Celda/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Celda.css";
 
-function Celda() {
+function Celda(props) {
     return(
         <button className="celda">
-
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.pokeClase + 1}.png`}></img>
         </button>
     );
 }
