bb3b7c5ff826d9fc0c1122cea3876e299d6db8fc
Irving Silva <54598135+IrViNg256@users.noreply.github.com>
2021-11-23 13:26:04 -0600

    Estados loading agregados

--- src/componentes/Lista/index.js
+++ src/componentes/Lista/index.js
@@ -4,28 +4,65 @@ import "./Lista.css";
 
 function Lista(props) {
     const [pokemon, setPokemon] = useState([]);
+    const [loadingState, setLoadingState] = useState("idle");
 
     useEffect(() => {
         const getPokemon = async() => {
-            const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
-            const data = await response.json();
-            const listaPokemon = data.results
-                .slice(0, data.results.length)
-                .map(items => items)
-                .flat();
-            setPokemon(listaPokemon);
+            setLoadingState("loading");
+            try {
+                const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+                const data = await response.json();
+                console.log(data.results);
+                const listaPokemon = data.results
+                    .slice(0, data.results.length)
+                    .map(items => items)
+                    .flat();
+                setPokemon(listaPokemon);
+                setLoadingState("complete");
+            } catch(error) {
+                setLoadingState("error");
+                console.log(error);
+            }
         }
         getPokemon();
     }, []);
 
+    function selectScreen() {
+        if(loadingState === "idle") {
+            return(
+                <section className="lista">
+                    <h2>Idle</h2>
+                </section>  
+            );
+        } else if (loadingState === "loading") {
+            return(
+                <section className="lista">
+                    <h2>Loading</h2>
+                </section>  
+            );
+        } else if (loadingState === "error") {
+            return(
+                <section className="lista">
+                    <h2>Error</h2>
+                </section>  
+            );
+        } else if (loadingState === "complete") {
+            return(
+                <section className="lista">
+                    {pokemon.map((pokeClase, id) => {
+                        return(
+                            <Celda key={id} pokeClase={id} handleOnClick={props.handleOnClick} />
+                        )
+                    })}
+                </section>
+            );
+        } else {
+            return(<h2>Empty</h2>);
+        }
+    }
+
     return(
-        <section className="lista">
-           {pokemon.map((pokeClase, id) => {
-               return(
-                   <Celda key={id} pokeClase={id} handleOnClick={props.handleOnClick} />
-               )
-           })}
-        </section>
+        selectScreen()
     );
 }
 
