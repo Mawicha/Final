4ffbfc0767187f926a5b0782ff76bffb9d48f0c2
Irving Silva <54598135+IrViNg256@users.noreply.github.com>
2021-11-23 17:27:04 -0600

    Más comentarios

--- src/componentes/Lista/index.js
+++ src/componentes/Lista/index.js
@@ -5,37 +5,37 @@ import "./Lista.css";
 function Lista(props) {
     const [pokemon, setPokemon] = useState([]); /* En este estado se guardará el listado de Pokémon obtenido de la llamada al API */
     const [loadingState, setLoadingState] = useState("idle"); /* Indica el estado del API, ya sea reposo, si se está haciendo la llamada y está cargando, si fue exitosa o hubo algún error durante la llamada */
-    const [searchState, setSearchState] = useState("");
+    const [searchState, setSearchState] = useState(""); /* Estado que guarda lo que se ha ingresado en la barra de búsqueda */
 
     useEffect(() => { /* Obtener la lista de Pokémon es un efecto, no una acción que inicie el usuario */
         const getPokemon = async() => { /* Se trata de una función asíncrona, se puede hacer algo más mientras se espera que se realice */
             setLoadingState("loading"); /* Establece el estado de carga a loading, el API está cargando */
             try { /* Función try, en caso de falla, el error es obtenido mediante un catch y la aplicación no se cae por completo */
-                let response;
-                if (searchState === "") {
-                    response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151"); /* Realiza la llamada al API, se usa await antes del fetch porque se espera una promesa, tipo de objeto propio de las funciones asíncronas */
-                } else {
-                    response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchState}`);
+                let response; /* Variable donde se guarda la respuesta promeso del API */
+                if (searchState === "") { /* En caso de que la barra de búsqueda esté vacía */
+                    response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151"); /* Realiza la llamada al API, se usa await antes del fetch porque se espera una promesa, tipo de objeto propio de las funciones asíncronas, se consigue el listado completo de Pokémon de la primera generación */
+                } else { /* En caso de que la barra de búsqueda tenga algo escrito */
+                    response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchState}`); /* Realiza la llamada al API con el nombre específico que está escrito en la barrade búsqueda */
                 }
                 const data = await response.json(); /* En esta línea se toma la respuesta del API y es leída por completo y su crea un objeto de JavaScript, se usa await porque el return es de tipo promesa también */
-                let listaPokemon;
-                if (searchState === ""){
+                let listaPokemon; /* Variable donde se guardara el arreglo de Pokémon */
+                if (searchState === ""){ /* En caso de que se regresen los 151 Pokémon, se tratarán los datos dentro del objeto a un arreglo */
                     listaPokemon = data.results /* data es un objeto, para acceder a los resultados de la búsqueda del API, se debe acceder a la propiedad results de este objeto data, el resultado es asignado a listaPokemon, pero antes hay otras funciones por realizar en las siguientes líneas */
                         .slice(0, data.results.length) /* Corta el arreglo que devuelve data.results desde el índice 0 hasta el último en su tamaño, en este caso 151, el último (151) no se incluye en el arreglo resultante, el original no se modifica, solo se crea uno nuevo */
                         .map(items => items) /* Mapea los elementos del arreglo para recordar sus posiciones originales */
                         .flat(); /* Se crea una nueva matriz con todos los elementos de los sub-arreglos concatenados */
-                } else {
+                } else { /* En caso contrario, solo inserta los datos del único Pokémon devuelto */
                     listaPokemon = data;
                 }
                 
-                setPokemon(listaPokemon);
-                setLoadingState("complete");
-            } catch(error) {
-                setLoadingState("error");
+                setPokemon(listaPokemon); /* Mete el listado de Pokémon dentro del estado pokemon */
+                setLoadingState("complete"); /* Cambia el estado de loading a complete, ya está listo para mostrarse */
+            } catch(error) { /* Atrapa el error si es que ocurre uno */
+                setLoadingState("error"); /* Cambia el estado de loading a error, para mostrar un mensaje de error */
             }
         }
-        getPokemon();
-    }, [searchState]);
+        getPokemon(); /* Llama a la función recien declarada anteriormente */
+    }, [searchState]); /* Este efecto se va a realizar cada que haya cambios en el estado searchState, el que guarda el contenido de la barra de búsqueda */
 
     function handleChange(event) {
         const value = event.target.value;
