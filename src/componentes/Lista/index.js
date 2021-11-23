0e523826ecadc496693f7d514ff10f3d0972f866
Irving Silva <54598135+IrViNg256@users.noreply.github.com>
2021-11-23 16:21:22 -0600

    Búsqueda a medio implementar

--- src/componentes/Lista/index.js
+++ src/componentes/Lista/index.js
@@ -11,7 +11,12 @@ function Lista(props) {
         const getPokemon = async() => { /* Se trata de una función asíncrona, se puede hacer algo más mientras se espera que se realice */
             setLoadingState("loading"); /* Establece el estado de carga a loading, el API está cargando */
             try { /* Función try, en caso de falla, el error es obtenido mediante un catch y la aplicación no se cae por completo */
-                const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151"); /* Realiza la llamada al API, se usa await antes del fetch porque se espera una promesa, tipo de objeto propio de las funciones asíncronas */
+                let response;
+                if (searchState === "") {
+                    response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151"); /* Realiza la llamada al API, se usa await antes del fetch porque se espera una promesa, tipo de objeto propio de las funciones asíncronas */
+                } else {
+                    response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchState}`);
+                }
                 const data = await response.json(); /* En esta línea se toma la respuesta del API y es leída por completo y su crea un objeto de JavaScript, se usa await porque el return es de tipo promesa también */
                 const listaPokemon = data.results /* data es un objeto, para acceder a los resultados de la búsqueda del API, se debe acceder a la propiedad results de este objeto data, el resultado es asignado a listaPokemon, pero antes hay otras funciones por realizar en las siguientes líneas */
                     .slice(0, data.results.length) /* Corta el arreglo que devuelve data.results desde el índice 0 hasta el último en su tamaño, en este caso 151, el último (151) no se incluye en el arreglo resultante, el original no se modifica, solo se crea uno nuevo */
@@ -24,7 +29,7 @@ function Lista(props) {
             }
         }
         getPokemon();
-    }, []);
+    }, [searchState]);
 
     function handleChange(event) {
         const value = event.target.value;
